872797a7b68c184b58eba0408a4349409b6c3d35
unknown <jcubero@paloalto.com.ec>
2021-04-16 12:06:54 -0500

    Footer estatico con Css

--- src/components/prueba_jc/Prueba1.jsx
+++ src/components/prueba_jc/Prueba1.jsx
@@ -1,36 +1,46 @@
 import React from 'react';
 import './Prueba1.css';
-import{BrowserRouter as Router,Route,Switch,Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 import about from "../productList/ProductList";
 
-function Prueba1(){
-return(
-    <Router>
-    <div className="Prueba1">
-        <header classname="App-header">
-            <ul>
-                <li>
-                    <Link to="/">Home</ Link>
-                </li>
-                <li>
-                    <Link to="/about">About </Link>
-                </li>
-                <li>
-                    <Link to="/user">User</ Link>
-                </li>
-            </ul>
-            <div>
-                <Switch>
-                    <Route exact path="/" /> 
-                    <Route exact path="/about" /> 
-                    <Route exact path="/user" /> 
-                </Switch>
-            </div>
-        </header>
-    </div>
-    </Router>
-);
+function Prueba1() {
+    return (
+
+
+        <html>
+            <Router>
+                <body>
+                <div>
+                    <Switch>
+                        <Route exact path="/" />
+                        <Route exact path="/about" component={about} />
+                        <Route exact path="/user" />
+                    </Switch>
+                </div>
+                </body>
+                <footer  class="pie">
+                <div>
+
+                    <ul>
+                        <li>
+                            <Link to="/">Home</ Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About </Link>
+                        </li>
+                        <li>
+                            <Link to="/user">User</ Link>
+                        </li>
+                        
+                    </ul>
+                  
+
+                </div>
+                </footer>
+            </Router>
+        </html >
+    );
 }
 
 export default Prueba1;
